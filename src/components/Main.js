a4a3304d70abab76e2dc842d18fc4e6addd01936
JaeHyunGround <gothddlek@naver.com>
2023-06-28 13:31:38 +0900

    feature : project setting

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/components/Main.css";
+
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Main;
