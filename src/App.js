5d0215877dc8a159837fdf9203777691bd86f14f
0Jaemin0 <jaemin2167@naver.com>
2023-07-03 00:15:52 +0900

    WavePage 구현

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './page/MainPage';
-import MakePage from './page/MakePage';
-import WritePage from './page/WritePage';
-import ReadPage from './page/ReadPage';
-import WavePage from './page/WavePage';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./page/MainPage";
+import MakePage from "./page/MakePage";
+import WritePage from "./page/WritePage";
+import ReadPage from "./page/ReadPage";
+import WavePage from "./page/WavePage";
+import { useState } from "react";
 
 function App() {
+  //WavePage Test
+  const [data, setData] = useState([
+    {
+      name: "AAA",
+      content: "Test1",
+      date: "2023-06-30",
+    },
+    {
+      name: "BBB",
+      content: "Test2",
+      date: "2023-06-31",
+    },
+    {
+      name: "CCC",
+      content: "Test3",
+      date: "2023-06-31",
+    },
+    {
+      name: "DDD",
+      content: "Test4",
+      date: "2023-06-31",
+    },
+  ]);
+
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path='/' element={<MainPage />}></Route>
-              <Route path='/makewave' element={<MakePage />}></Route>
-              <Route path='/writewave' element={<WritePage />}></Route>
-              <Route path='/readwave' element={<ReadPage />}></Route>
-              <Route path='/wave' element={<WavePage />}></Route>
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/makewave" element={<MakePage />}></Route>
+        <Route path="/writewave" element={<WritePage />}></Route>
+        <Route path="/readwave" element={<ReadPage />}></Route>
+        <Route path="/wave" element={<WavePage List={data} />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/page/WavePage.js
+++ src/page/WavePage.js
@@ -1,12 +1,118 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
 import "../styles/page/WavePage.css";
+import WaveCp from "../components/wave/WaveCp.js";
 
-const WavePage = () => {
-    return (
-        <div>
-            wave
+//(주인이름, url) and List = (보낸이, 편지글, 날짜)
+const WavePage = ({ List }) => {
+  const [showUrl, setShowUrl] = useState(false);
+  const scrollContainerRef = useRef(null);
+
+  const openUrl = () => {
+    setShowUrl(true);
+  };
+
+  const closeUrl = () => {
+    setShowUrl(false);
+  };
+
+  //모바일 지원 X => 다른 방법 or 사용자가 직접 복사
+  const copyUrl = () => {
+    const url = "aa"; //url 작성
+    navigator.clipboard.writeText(url);
+    alert("복사 되었습니다");
+  };
+
+  const pageCount = Math.ceil(List.length / 3); //생성할 페이지 수
+
+  //페이지 오른쪽 왼쪽 이동
+  const moveRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        top: 0,
+        left: 365,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const moveLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({
+        top: 0,
+        left: -365,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  //페이지 생성
+  const addPage = () => {
+    const pages = [];
+
+    for (let i = 0; i < pageCount; i++) {
+      const startIndex = i * 3;
+      const endIndex = (i + 1) * 3;
+
+      const pageItems = List.slice(startIndex, endIndex).map((e, index) => (
+        <WaveCp
+          index={startIndex + index}
+          name={e.name}
+          content={e.content}
+          date={e.date}
+        />
+      ));
+
+      const page = (
+        <div className="Wave">
+          <div className="Name">
+            <h4>홍길동</h4>의 바다
+          </div>
+          <div className="waveImg">
+            <img src="/img/dol.png" className="dolImg" alt="돌고래"></img>
+            <img src="/img/boat.png" className="boatImg" alt="보트"></img>
+            <img src="/img/tube.png" className="tubeImg" alt="튜브"></img>
+            <div className="Letter">
+              <button onClick={moveLeft} className="leftBtn"></button>
+              {/* 편지 띄우기 */}
+              {pageItems}
+              <button onClick={moveRight} className="rightBtn"></button>
+            </div>
+            <div className="Share">
+              <button className="shareBtn" onClick={openUrl}>
+                <img src="/img/share.png" alt="공유하기 이미지" />
+                공유하기
+              </button>
+              {showUrl && (
+                <div className="urlPage">
+                  <div className="url">생성한 URL 주소</div>
+                  <div className="urlBtn">
+                    <button className="copyBtn" onClick={copyUrl}>
+                      복사하기
+                    </button>
+                    <button className="backBtn" onClick={closeUrl}>
+                      뒤로가기
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-    );
+      );
+      pages.push(page);
+    }
+    return pages;
+  };
+
+  return (
+    <div
+      className="WaveContainer"
+      style={{ width: `${100 * pageCount}%` }}
+      ref={scrollContainerRef}
+    >
+      {addPage()}
+    </div>
+  );
 };
 
 export default WavePage;
